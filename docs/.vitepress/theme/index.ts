36b9c04cd857ca5a0dca8fb24f950c3a622d2cad
yuechao.xu <yuechao.xu@appshahe.com>
2023-04-28 14:08:44 +0800

    feat: 添加图片搜索功能

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,5 +1,25 @@
 import VPTheme from 'metaapp-prodigytech-doc-theme'
+import { useRoute } from 'vitepress'
+import { watch } from 'vue'
+import mediumZoom, { Zoom } from 'medium-zoom'
+import './index.css'
 
 export default {
-  ...VPTheme
+  ...VPTheme,
+  setup() {
+    const route = useRoute()
+    let zoom: Zoom | undefined
+    watch(
+      route,
+      () => {
+        zoom?.detach()
+        setTimeout(() => {
+          zoom = mediumZoom('img', { background: 'var(--vp-c-bg)' })
+        })
+      },
+      {
+        immediate: true
+      }
+    )
+  }
 }
