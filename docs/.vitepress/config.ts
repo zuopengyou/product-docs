bd44d709e596ed1579e558ef9dc67a028bad7f5b
yuechao.xu <yuechao.xu@appshahe.com>
2023-05-04 10:12:43 +0800

    fix: 修复跳转搜索页面路径不对的问题

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -188,9 +188,6 @@ export default defineConfigWithTheme<DefaultTheme.Config>({
       }
     ],
     algolia: {
-      // appId: '89BNK6UU0A',
-      // apiKey: 'f691939e4fa8b414f92c84c288d2097a',
-      // indexName: 'all-docs',
       appId: 'I2PHYUBLCN',
       apiKey: '62ee775311415d26549e0e30fef5aa38',
       indexName: 'api-docs_prodigytech',
@@ -213,7 +210,8 @@ export default defineConfigWithTheme<DefaultTheme.Config>({
             name: 'API文档'
           }
         ]
-      }
+      },
+      searchPage: 'https://search.ark.online/#/search'
     },
     siteTitle: '产品手册',
     nav: [
