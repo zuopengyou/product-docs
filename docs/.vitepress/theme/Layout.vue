764d40d91ff026ea803ce93f997431eb0512e32b
yuechao.xu <yuechao.xu@appshahe.com>
2024-01-24 18:01:13 +0800

    feat: 去掉 ask ai

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -1,14 +1,10 @@
 <!-- Index.vue -->
 <script setup>
 import VPTheme from 'doc-theme-323'
-import Documate from '@documate/vue'
 const { Layout } = VPTheme
 </script>
 <template>
   <Layout>
-    <template #nav-bar-content-before>
-      <Documate endpoint="https://38wnp18uy7.us.aircode.run/ask" />
-    </template>
     <template #doc-copyright>
       <div style="margin-top: 20px">
         Copyright © 2023 ark.online All Rights Reserved |
